44c5d6dc0aa7c8923670c6a566de42e1ea55322b
Kaique Moraes <80864991+kaique-moraees@users.noreply.github.com>
2024-11-28 00:05:40 -0300

    Update CorsConfig.java

--- src/main/java/com/example/catalogofilmes/config/CorsConfig.java
+++ src/main/java/com/example/catalogofilmes/config/CorsConfig.java
@@ -1,25 +1,31 @@
 package com.example.catalogofilmes.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                // Libera o CORS para todas as origens
-                registry.addMapping("/**") 
-                        .allowedOrigins("*")  // Permite todas as origens
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Permite todos os métodos
-                        .allowedHeaders("*")  // Permite todos os cabeçalhos
-                        .allowCredentials(true);  // Permite enviar cookies com a requisição (se necessário)
-            }
-        };
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // Permite todas as origens
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        // Permite todos os métodos HTTP
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        // Permite todos os cabeçalhos
+        corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+        // Permite a exposição de cabeçalhos personalizados
+        corsConfiguration.setExposedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsFilter(source);
     }
 }
